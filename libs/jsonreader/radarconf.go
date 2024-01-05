81c3428385753d314f06b72ed31a867e7e973272
Andrea Vaccaro <vaccaro.andrea45@gmail.com>
2024-01-05 20:15:54 +0100

    adding radar config file reader

---
+++ libs/jsonreader/radarconf.go
@@ -0,0 +1,24 @@
+package jsonreader
+
+import (
+	"bytes"
+	"encoding/json"
+	"os"
+)
+
+func ReadRadarConf() (RadarConf, bool) {
+	path, err := os.Getwd()
+	if err != nil {
+		return RadarConf{}, true
+	}
+	text, err := os.ReadFile(path + "/config/radarconf.json")
+	if err != nil {
+		return RadarConf{}, true
+	}
+	text = bytes.ReplaceAll(text, []byte{13, 10}, []byte{})
+	var conf RadarConf
+	if err := json.Unmarshal(text, &conf); err != nil {
+		return RadarConf{}, true
+	}
+	return conf, false
+}
