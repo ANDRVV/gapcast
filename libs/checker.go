e29faae8b19511d71591a7bd446c7887fccd66cd
Andrea Vaccaro <vaccaro.andrea45@gmail.com>
2024-02-11 19:58:33 +0100

    '-' is unsupported for mac, changing regex

--- libs/checker.go
+++ libs/checker.go
@@ -51,7 +51,7 @@ func IsValid(data string) bool {
 }
 
 func IsValidMAC(mac string) bool {
-	return regexp.MustCompile("^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$").MatchString(mac)
+	return regexp.MustCompile("^([0-9A-Fa-f]{2}[:]){5}([0-9A-Fa-f]{2})$").MatchString(mac)
 }
 
 func WriterCheck(file string) bool {
