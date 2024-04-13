8064492f16beb0fd1dd0ae2f788bb107106604ec
Andrea Vaccaro <vaccaro.andrea45@gmail.com>
2024-04-13 17:50:19 +0200

    updated to new version of RadarRSSI

--- libs/RadarRSSI/RadarRSSI.go
+++ libs/RadarRSSI/RadarRSSI.go
@@ -1,9 +1,8 @@
 package RadarRSSI
 
 import (
+	"RadarRSSI/libs"
 	"errors"
-	"gapcast/libs/RadarRSSI/libs"
-	"math"
 )
 
 func GetRFDataFromRadiotap(packetData []byte, RXAntennaDBI float64) (libs.RFData, error) {
@@ -19,7 +18,7 @@ func GetCustomRFData(ReceivedDBM float64, Channel int, RXAntennaDBI float64) lib
 }
 
 func GetDefaultTransmitterData() libs.TransmitterData {
-	return libs.TransmitterData{TXAntennaDBI: 3, TXPowerDBM: 20.5}
+	return libs.TransmitterData{TXAntennaDBI: 4, TXPowerDBM: 17.5}
 }
 
 func GetCustomTransmitterData(TXAntennaDBI float64, TXPowerDBM float64) libs.TransmitterData {
@@ -27,41 +26,22 @@ func GetCustomTransmitterData(TXAntennaDBI float64, TXPowerDBM float64) libs.Tra
 }
 
 func GetAutoDBPathLoss(RFData libs.RFData) (DBPathLoss float64) {
-	if RFData.Channel < 15 {
-    	var autoDBPL float64 = 0.65 * math.Abs(RFData.ReceivedDBM) + -12  
-		if autoDBPL > 10 {
-			return autoDBPL
+	if RFData.ReceivedDBM > -8 {
+		return 0
+	} else {
+		if RFData.Channel < 15 {
+			return 0.65 * (-RFData.ReceivedDBM) - 12  
 		}
-		return 10
-	}
-	var autoDBPL float64 = 0.5555555555555556 * math.Abs(RFData.ReceivedDBM) + -8.222222222222221 
-	if autoDBPL > 2 {
-		return autoDBPL
-	}
-	return 2 
+		return 0.55 * (-RFData.ReceivedDBM) - 8.22
+	} 
 }
 
 func Radiolocate(RFData libs.RFData, TXData libs.TransmitterData, DBPathLoss float64) (meters float64) {
-	var totalPathLoss func() float64 = func() float64 {
-		if DBPathLoss > 0 {
-			return DBPathLoss
-		} else {
-			if RFData.Channel < 15 {
-				return 10
-			} else {
-				return 2
-			}
-		}
-	}
 	var info []float64 
 	info = append(info, RFData.RXAntennaDBI)
 	info = append(info, TXData.TXAntennaDBI)
 	info = append(info, TXData.TXPowerDBM)
 	info = append(info, float64(RFData.Channel))
-	if RFData.Channel < 15 {
-		info = append(info, RFData.ReceivedDBM + totalPathLoss())
-	} else {
-		info = append(info, RFData.ReceivedDBM + totalPathLoss())
-	}
+	info = append(info, RFData.ReceivedDBM - DBPathLoss)
 	return libs.CalculateFriis(info)
 }
--- libs/RadarRSSI/libs/calc.go
+++ libs/RadarRSSI/libs/calc.go
@@ -14,16 +14,7 @@ var (
 )
 
 func CalculateFriis(info []float64) float64 { 							   	
-	var numerator float64 = Prod(info[:len(info) - 2]) * math.Pow(299792458, 2)
-	var semiDenominator float64 = 4 * math.Pi * GetLinearFrequency(int(info[len(info) - 2]))
-	var meters float64 = 0.1
-	for {
-		if 10 * math.Log10(numerator / math.Pow(semiDenominator * meters, 2)) < info[len(info) - 1] {
-			if meters > 0.1 {
-				return toF64(fmt.Sprintf("%.1f", meters - 0.1))
-			}
-			return toF64(fmt.Sprintf("%.1f", meters))
-		}
-		meters += 0.1
-	}
+	var numerator float64 = math.Sqrt((Prod(info[:len(info) - 2]) * math.Pow(299792458, 2)) / (math.Pow(10, info[len(info) - 1] / 10)))                   
+	var denominator float64 = 4 * math.Pi * (GetLinearFrequency(int(info[len(info) - 2])))
+	return toF64(fmt.Sprintf("%.1f", numerator / denominator))		
 }
