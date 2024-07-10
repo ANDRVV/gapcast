fb981dc0557cd1a772067346228cf951f1698b78
Andrea Vaccaro <vaccaro.andrea45@gmail.com>
2024-07-11 01:01:56 +0200

    update code formatting

--- libs/msg.go
+++ libs/msg.go
@@ -5,22 +5,22 @@ import (
 	"time"
 )
 
-func Log(color Colors, msg string) {
-	fmt.Println(color.White + "[" + color.Yellow + time.Now().Format("15:04:05") + color.White + "] [" + color.Blue + "LOG" + color.White + "] " + msg)
-}
-
-func Error(color Colors, msg string) {
-	fmt.Println(color.White + "[" + color.Red + "ERROR" + color.White + "] " + msg)
-}
-
 func CustomLog(color Colors, titleColor string, title string, msg string) {
-	fmt.Println(color.White + "[" + color.Yellow + time.Now().Format("15:04:05") + color.White + "] [" + titleColor + title + color.White + "] " + msg)
+	fmt.Printf("%s[%s%s%s] [%s%s%s] %s\n", color.White, color.Yellow, time.Now().Format("15:04:05"), color.White, titleColor, title, color.White, msg)
 }
 
 func NOTIMECustomLog(color Colors, titleColor string, title string, msg string) {
-	fmt.Println(color.White + "[" + titleColor + title + color.White + "] " + msg)
+	fmt.Printf("%s[%s%s%s] %s\n", color.White, titleColor, title, color.White, msg)
+}
+
+func Log(color Colors, msg string) {
+	CustomLog(color, color.Blue, "LOG", msg)
+}
+
+func Error(color Colors, msg string) {
+	NOTIMECustomLog(color, color.Red, "ERROR", msg)
 }
 
 func Warning(color Colors, msg string) {
-	fmt.Println(color.White + "[" + color.Yellow + "WARNING" + color.White + "] " + msg)
+	NOTIMECustomLog(color, color.Yellow, "WARNING", msg)
 }
